dbb9e7e4dd06f2ca836e52cd8833df6c33e0cef1
Кирилл Аксенов <kirill.ax@tprofeng.ru>
2021-11-30 19:57:50 +0300

    Добавьте файлы проекта.

---
+++ Task4-2-2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4_2
+{
+    class Program
+    {
+        //Сравнить количество положительных и отрицательных чисел
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите последовательность целых чисел");
+            int counterPositive = 0;
+            int counterNegative = 0;
+            int buffer;
+            do
+            {
+                buffer = Convert.ToInt32(Console.ReadLine());
+                if (buffer > 0)
+                {
+                    counterPositive += 1;
+                }
+                else
+                {
+                    if (buffer < 0)
+                    {
+                        counterNegative += 1;
+                    }  
+                }
+            } while (buffer != 0);
+            if (counterPositive > counterNegative)
+            {
+                Console.WriteLine("Количество положительных чисел больше {0} > {1}", counterPositive, counterNegative);
+            }
+            else
+            {
+                if (counterPositive < counterNegative)
+                {
+                    Console.WriteLine("Количество отрицательных чисел больше {0} > {1}", counterNegative, counterPositive);
+                }
+                else
+                {
+                    Console.WriteLine("Количество положительных и отрицательных чисел равно {0} равно {1}", counterNegative, counterPositive);
+                }                
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task4-2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4_2
+{
+    class Program
+    {
+        //Сравнить количество положительных и отрицательных чисел
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите последовательность целых чисел");
+            string a = Console.ReadLine();
+            int b=Convert.ToInt32(a);
+            int i = 0, p = 0,c, n = 0,d;
+
+            do
+            {
+                Console.Write("Введите" + i + "число-");
+                c = Convert.ToInt32(Console.ReadLine());
+                d= Convert.ToInt32(Console.ReadLine());
+                if (c > 0)
+                    p++;
+                if (d < 0)
+                    n++;
+            } while (i!=0&&i<=b);
+            Console.WriteLine("Количество положительных=" +p);
+            Console.WriteLine("Количество отрицательных=" + n);
+            Console.ReadKey();
+        }
+    }
+}
